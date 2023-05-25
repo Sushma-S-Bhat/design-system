f319964555187c9c041791ab11721902d1359a35
sushma bhat <sushma@aglinthq.com>
2023-05-25 16:28:55 +0530

    changed yml

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,39 +1,23 @@
 import { Avatar as MuiAvatar } from '@mui/material'
 import React from 'react'
 
-export const Avatar = ({ variant='contained', color, size, label, ...props }) => {
+export const Avatar = ({ variant='circular', size, ...props }) => {
     // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
     return (
-      <MuiAvatar variant={variant} size={size} color={color} >{label}</MuiAvatar>
+      <MuiAvatar variant={variant} size={size}>{label}</MuiAvatar>
     );
   };
   
-  Button.propTypes = {
-    /**
-     * Is this the principal call to action on the page?
-     */
-    primary: PropTypes.bool,
-    /**
-     * What background color to use
-     */
-    backgroundColor: PropTypes.string,
-    /**
-     * How large should the button be?
-     */
+  Avatar.propTypes = {
+   
+    variant: PropTypes.string,
+  
     size: PropTypes.oneOf(['small', 'medium', 'large']),
-    /**
-     * Button contents
-     */
-    label: PropTypes.string.isRequired,
-    /**
-     * Optional click handler
-     */
-    onClick: PropTypes.func,
+    
   };
   
   Avatar.defaultProps = {
-    backgroundColor: null,
-    primary: false,
+    variant:'circular',
     size: 'medium',
     onClick: undefined,
   };
