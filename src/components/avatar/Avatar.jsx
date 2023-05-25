403bbcaa87bd3b7241a956b27de668fe4a867d24
sushma bhat <sushma@aglinthq.com>
2023-05-25 16:44:59 +0530

    changed avatar story file

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,5 +1,7 @@
 import { Avatar as MuiAvatar } from '@mui/material'
 import React from 'react'
+import PropTypes from 'prop-types';
+
 
 export const Avatar = ({ variant='circular', size, ...props }) => {
     // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
